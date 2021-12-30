69f7ac2b30c51d15e1db50f8d2807557fd3e7e62
ckenlin <96581920+ckenlin@users.noreply.github.com>
2021-12-30 22:03:26 +0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -33,7 +33,7 @@ def inputPlayerLetter():
         st.write('Do you want to be X or O?')
         #letter = st.sider.text_input('please input X or O?' , 'X O')
         #letter = st.sidebar.text_input('please input X or O?' , 'X O')
-        letter = st.sidebar.text_input('please input X or O?' , 'X O')
+        letter = st.button.text_input('please input X or O?' , 'X O')
         letter = letter.upper()
     
     # the first element in the list is the player’s letter, the second is the computer's letter.
